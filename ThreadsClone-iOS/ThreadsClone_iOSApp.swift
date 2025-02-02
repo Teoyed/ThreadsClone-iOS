c2fb0e45c7ebeacdff21869b9e9b82a58026b052
Reza Ahmadizadeh <mr.ahmadizadeh28@gmail.com>
2025-02-02 03:32:21 +0330

    Initial Commit

---
+++ ThreadsClone-iOS/ThreadsClone_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsClone_iOSApp.swift
+//  ThreadsClone-iOS
+//
+//  Created by Reza Ahmadizadeh on 11/14/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsClone_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsClone-iOSTests/ThreadsClone_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsClone_iOSTests.swift
+//  ThreadsClone-iOSTests
+//
+//  Created by Reza Ahmadizadeh on 11/14/1403 AP.
+//
+
+import Testing
+@testable import ThreadsClone_iOS
+
+struct ThreadsClone_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
