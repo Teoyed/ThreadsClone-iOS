7746f4bc0bf41a8421e255db9ef6d95da6a331d0
Reza Ahmadizadeh <mr.ahmadizadeh28@gmail.com>
2025-02-05 10:30:23 +0330

    Edit Profile UI

---
+++ ThreadsClone-iOS/Core/Profile/EditProfile/Views/EditProfileView.swift
@@ -0,0 +1,97 @@
+//
+//  EditProfileView.swift
+//  ThreadsClone-iOS
+//
+//  Created by Reza Ahmadizadeh on 11/17/1403 AP.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .edgesIgnoringSafeArea([.bottom, .horizontal])
+                
+                VStack {
+                    // name and profile image
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            
+                            Text("Charles Leclerc ")
+                        }
+                        
+                        Spacer()
+                        
+                        CircularProfileImageView()
+                    }
+                    
+                    Divider()
+                    
+                    // bio field
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Enter your bio...", text: $bio, axis: .vertical)
+                    }
+                    
+                    Divider()
+                    
+                    // link
+                    VStack(alignment: .leading) {
+                        Text("link")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Add Link...", text: $link)
+                    }
+                    
+                    Divider()
+                    
+                    // private or not
+                    Toggle("Private Profile", isOn: $isPrivateProfile)
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
